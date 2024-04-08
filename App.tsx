47f6df79a638fba3e95806684295766af80990f7
Thais Cristine <this.cristine06@gmail.com>
2024-04-09 00:02:11 +0200

    Initial commit daily diet

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+
+
+import { ThemeProvider } from 'styled-components/native';
+import theme from './src/theme';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { Loading } from '@components/Loading';
 
 export default function App() {
+
+  const [fontLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle="light-content" 
+        backgroundColor='transparent'
+        translucent
+      />
+      {/* {fontLoaded ? <Routes /> : <Loading />} */}
+      <Loading /> 
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
